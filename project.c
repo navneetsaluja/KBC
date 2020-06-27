ea4998f64f4ce27f681c14b2ea4c8e8d288772de
Navneet Saluja <saluja.navneet2002@gmail.com>
2020-06-28 00:19:05 +0530

    Small changes for better GUI.

--- project.c
+++ project.c
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <ctype.h>
 #include <string.h>
+#include <locale.h>
 
 struct question
 {
@@ -17,10 +18,18 @@ void printQuestion(FILE *infile, int *array, int i);
 void checkAnswer(struct question ques);
 void fiftyfifty(struct question ques);
 void flipQuestion();
+int calculateGameOver(int roundNumber);
+int calculateGameQuit(int roundNumber);
+
+/* Global variables to determine whether a lifeline is used / whether Game is over due to a wrong answer */
+int isGameOver = 0, isGameQuit, isFiftyFiftyUsed = 0, isFlipUsed = 0;
 
 int main()
 
 {
+    /* Helps in formatting numbers with commas in Indian currency style. */
+    setlocale(LC_NUMERIC, "en_IN");
+
     /* FILE OPENING */
     FILE *infile;
     infile = fopen("questions.txt", "r");
@@ -49,19 +58,39 @@ int main()
     printf("There are total 15 stages (questions) in this game where question no. 5 and 10 are milestone questions.\n");
     printf(" 15) 1,00,00,000\n 14)   50,00,000\n 13)   25,00,000\n 12)   12,50,000\n 11)    6,40,000\n 10)    3,20,000\n 09)    1,60,000\n 08)      80,000\n 07)      40,000\n 06)      20,000\n 05)      10,000\n 04)       5,000\n 03)       3,000\n 02)       2,000\n 01)       1,000\n");
     printf("You have to answer the given question correctly to see and play the next question.\n");
-    printf("As long as you have only seen the question and its four possible answers, you can always quit the game.\nat any stage and keep the credits you have already won.\n");
+    printf("As long as you have only seen the question and its four possible answers, you can always quit the game by using ' Q ' key,\nat any stage and keep the credits you have already won.\n");
     printf("If you choose to answer a question and answer it incorrectly, you will lose all the credits and will\nonly take away credits you have earned until last milestone question. That is:\n");
     printf(" If incorrect answer was for question from 1 to 5, you will get 0 credits.\n If incorrect answer was for question from 6 to 10, you will get 10,000 credits.\n If incorrect answer was for question from 11 to 15, you will get 3,20,000 credits. \n");
-    printf("You have two lifelines:\n First is, 50-50: Two wrong options will be removed from the screen.\n And second is, Flip the question: The question will be replaced by an entire new question .");
+    printf("You have two lifelines:\n First is, 50-50: Two wrong options will be removed from the screen. It can be used by pressing ' F ' key.\n And second is, Flip the question: The question will be replaced by an entire new question. It can be used by pressing ' L ' key.\n");
 
-    printf("Press Enter to start the quiz!");
-    getchar();
+    printf(" Press Enter to start the quiz!");
+    while (getchar() != 10)
+    {
+        ;
+    }
     system("clear");
-
     /* MAIN PART */
-    for (int i = 0; i < 15; ++i)
+    int i;
+    for (i = 0; i < 15; ++i)
     {
         printQuestion(infile, num, i);
+
+        if (isGameOver == 1)
+        {
+            printf("Your game is over and you have won %'d credits.\n", calculateGameOver(i + 1));
+            break;
+        }
+
+        if (isGameQuit == 1)
+        {
+            printf("You have quit the game and won %'d credits. Congrats!\n", calculateGameQuit(i + 1));
+            break;
+        }
+    }
+
+    if (i == 15)
+    {
+        printf("Congrats, you have won Kaun Banega Crorepati and 1 Crore credits!");
     }
 
     fclose(infile);
@@ -94,6 +123,7 @@ void printQuestion(FILE *infile, int *array, int i)
     /* DISPLAYING A QUESTION */
 display:
     printf("Question Number %d\n", i + 1);
+    printf("You have %'d credits and you are playing for %'d credits.\n\n", calculateGameQuit(i + 1), calculateGameQuit(i + 2));
     printf("%s\n", q.question);
     printf("A. %s \t\t B. %s\n", q.options[0], q.options[1]);
     printf("C. %s \t\t D. %s\n\n", q.options[2], q.options[3]);
@@ -106,9 +136,27 @@ void checkAnswer(struct question ques)
     char opt;
     int j;
 
-    printf("Enter the option you wish to choose: ");
+    printf("Enter the option you wish to choose: \nA, B, C and D for the respective options. Q to quit the game.\n");
+
+    if (isFiftyFiftyUsed == 0)
+    {
+        printf("You have the Fifty Fifty lifeline left. Enter F to use the lifeline.\n\n");
+    }
+
+    if (isFlipUsed == 0)
+    {
+        printf("You have Flip the Question lifeline left. Enter L to use the lifeline.\n\n");
+    }
+
+    if ((isFlipUsed == 1) && (isFiftyFiftyUsed == 1))
+    {
+        printf("You have no lifelines left.");
+    }
+
     scanf("%c", &opt);
-    getchar();
+    while (getchar() != 10)
+    {
+    }
     switch (tolower(opt))
     {
     case 'a':
@@ -124,30 +172,62 @@ void checkAnswer(struct question ques)
         j = 3;
         break;
     case 'f':
-        /* FIFTY FIFTY LIFELINE */
-        fiftyfifty(ques);
-        goto skip;
+        /* Fifty Fifty lifeline. */
+        if (isFiftyFiftyUsed == 1)
+        {
+            printf("Sorry you have already used this lifeline. Please re enter with another option.");
+            checkAnswer(ques);
+            goto skip;
+        }
+        else
+        {
+            fiftyfifty(ques);
+            goto skip;
+        }
+
     case 'l':
-        flipQuestion();
+        /* Flip the Question lifeline. */
+        if (isFlipUsed == 1)
+        {
+            printf("Sorry you have already used this lifeline. Please re enter with another option.");
+            checkAnswer(ques);
+            goto skip;
+        }
+        else
+        {
+            flipQuestion();
+            goto skip;
+        }
+
+    case 'q':
+        isGameQuit = 1;
         goto skip;
 
     default:
     {
-        printf("WRONG OPTION ENTERED. RE ENTER.\n");
+        printf("Wrong option entered. Re enter.\n");
         checkAnswer(ques);
     };
     }
 
     if (strcmp(ques.correctAnswer, ques.options[j]) == 0)
-        printf("CORRECT ANSWER\n\n");
+    {
+        system("clear");
+        printf("Correct answer. Moving on to the next round.\n\n");
+    }
     else
-        printf("WRONG\n\n");
-
+    {
+        printf("Wrong answer.\n");
+        isGameOver = 1;
+    }
 skip:;
 }
 
 void fiftyfifty(struct question ques)
 {
+    isFiftyFiftyUsed = 1;
+    system("clear");
+
     char tempstr[4][40];
     int temparr[4], count = 0;
 
@@ -179,8 +259,92 @@ void fiftyfifty(struct question ques)
 
 void flipQuestion()
 {
+    isFlipUsed = 1;
+    system("clear");
     printf("%s\n", flip.question);
     printf("A. %s \t\t B. %s\n", flip.options[0], flip.options[1]);
     printf("C. %s \t\t D. %s\n\n", flip.options[2], flip.options[3]);
     checkAnswer(flip);
+}
+
+int calculateGameOver(int roundNumber)
+{
+    if (roundNumber <= 5)
+        return 0;
+    else if (roundNumber <= 10)
+        return 10000;
+    else
+        return 320000;
+}
+
+int calculateGameQuit(int roundNumber)
+{
+    switch (roundNumber)
+    {
+    case 1:
+        return 0;
+        break;
+
+    case 2:
+        return 1000;
+        break;
+
+    case 3:
+        return 2000;
+        break;
+
+    case 4:
+        return 3000;
+        break;
+
+    case 5:
+        return 5000;
+        break;
+
+    case 6:
+        return 10000;
+        break;
+
+    case 7:
+        return 20000;
+        break;
+
+    case 8:
+        return 40000;
+        break;
+
+    case 9:
+        return 80000;
+        break;
+
+    case 10:
+        return 160000;
+        break;
+
+    case 11:
+        return 320000;
+        break;
+
+    case 12:
+        return 640000;
+        break;
+
+    case 13:
+        return 1250000;
+        break;
+
+    case 14:
+        return 2500000;
+        break;
+
+    case 15:
+        return 5000000;
+        break;
+
+    case 16:
+        return 10000000;
+        break;
+    default:
+        break;
+    }
 }
